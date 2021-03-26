0c82efb092ec1b73b4cc48dab15014ff1238feb7
Luis Carranza <carranlu@gmail.com>
2021-03-25 18:58:20 -0600

    Initial Commit

---
+++ FirebaseInteraction/FirebaseInteractionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseInteractionApp.swift
+//  FirebaseInteraction
+//
+//  Created by Luis Javier Carranza on 25/03/21.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseInteractionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
